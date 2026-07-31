ce8f2dd21798f798a117da4f936f937bd9581fce
Fatima Tahir <adan4528858@gmail.com>
2026-07-31 21:46:52 +0500

    Added vercel.json for deployment

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -53,7 +53,8 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowReactApp",
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173")
+            policy.WithOrigins("http://localhost:5173", "http://localhost:5012",                          // Local backend port fallback
+                    "https://scriblia-stationery.vercel.app/")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
